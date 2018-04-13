7606c36d5caab1c5ddac9d66c9882c46f6f6f45c
Dinh Trieu <nguyendinhtrieu1996@gmail.com>
2018-04-13 16:10:50 +0700

    Update Server Side

--- ServerSocket/src/serversocket/Chessboard.java
+++ ServerSocket/src/serversocket/Chessboard.java
@@ -10,5 +10,51 @@ package serversocket;
  * @author dinhtrieu
  */
 public class Chessboard {
-    
+   public static int colQty = 8;
+   public static int rowQty = 8;
+   private ChessboardState[][] chessboard;
+   private RolePlayer winner = null;
+   private int checkedCount;
+   private Boolean isGameOver;
+   private RolePlayer currentPlayer;
+
+    public Chessboard() {
+        this.init();
+    }
+   
+   private void init() {
+       checkedCount = 0;
+       isGameOver = false;
+       chessboard = new ChessboardState[rowQty][colQty];
+       
+       for (int i = 0; i < rowQty; ++i) {
+           for (int j = 0; j < colQty; ++j) {
+               chessboard[i][j] = ChessboardState.NULL;
+           }
+       }
+   }
+   
+   //Public method
+   public Boolean makeMove(Move move) {
+       if (!canMove(move)) {
+           return false;
+       }
+       
+       return null;
+   }
+   
+   public Boolean checkWin(Move move) {
+       return null;
+   }
+   
+   public Boolean canMove(Move move) {
+       return null;
+   }
+   
+   public boolean isGameOver(RolePlayer player){
+        
+
+        return false;
+    }
+   
 }
--- ServerSocket/src/serversocket/Message.java
+++ ServerSocket/src/serversocket/Message.java
@@ -15,4 +15,51 @@ public class Message {
     private PlayerClient toPlayer;
     private StatusCode statusCode;
     private Move move;
+
+    public Message() {
+    }
+
+    public Message(PlayerClient toPlayer, StatusCode statusCode, Move move) {
+        this.toPlayer = toPlayer;
+        this.statusCode = statusCode;
+        this.move = move;
+    }
+    
+    public String getMessageContent() {
+        String result = "";
+        
+        result += statusCode.getRawValue() + "";
+        result += "-" + toPlayer.getRolePlayer().getRawValue();
+        result += "-" + move.getRow();
+        result += "-" + move.getCol();
+        
+        return result;
+    }
+
+    public PlayerClient getToPlayer() {
+        return toPlayer;
+    }
+
+    public void setToPlayer(PlayerClient toPlayer) {
+        this.toPlayer = toPlayer;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(StatusCode statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    public void setMove(Move move) {
+        this.move = move;
+    }
+    
+    
+    
 }
